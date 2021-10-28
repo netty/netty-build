59144eb0469434acab33e255b2ae47e0f4dd0c2b
Alex Chekunkov <alex.chekunkov@databricks.com>
2021-10-27 18:09:38 -0700

    Remove * import

--- common/src/main/java/io/netty/build/checkstyle/StaticFinalBufferCheck.java
+++ common/src/main/java/io/netty/build/checkstyle/StaticFinalBufferCheck.java
@@ -16,7 +16,11 @@
 
 package io.netty.build.checkstyle;
 
-import com.puppycrawl.tools.checkstyle.api.*;
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.FullIdent;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 import java.util.regex.Pattern;
 
